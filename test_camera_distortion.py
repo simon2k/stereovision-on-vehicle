b73f5590ae0d4e42a758bf738fc4125831b2d6c0
Szymon Kieloch <skieloch@gmail.com>
2020-01-06 22:57:13 +0100

    Add script to test camera distortion

--- test_camera_distortion.py
+++ test_camera_distortion.py
@@ -1,31 +1,31 @@
 import cv2
 import numpy as np
 
-PHOTOS_PATH = 'data/photos/stereo/640x360'
+PHOTOS_PATH = 'data/photos/'
 
 right_calibration = np.load('data/calibration/right-camera-calibration.npz')
 right_camera_matrix = right_calibration['camera_matrix']
 right_dist_coeffs = right_calibration['dist_coeffs']
 
-right_img = cv2.imread(f'{PHOTOS_PATH}/right-10.jpg')
+right_img = cv2.imread(f'{PHOTOS_PATH}/calibration_test.png')
 
 right_img_corr = cv2.undistort(right_img, cameraMatrix=right_camera_matrix, distCoeffs=right_dist_coeffs)
 
-cv2.imshow('right - original', right_img)
-cv2.imshow('right - corrected', right_img_corr)
-cv2.imshow('right - diff', right_img_corr - right_img)
+cv2.imshow('right-original', right_img)
+cv2.imshow('right-corrected', right_img_corr)
+cv2.imshow('right-diff', (255 - (right_img - right_img_corr)))
 
 left_calibration = np.load('data/calibration/left-camera-calibration.npz')
 left_camera_matrix = left_calibration['camera_matrix']
 left_dist_coeffs = left_calibration['dist_coeffs']
 
-left_img = cv2.imread(f'{PHOTOS_PATH}/left-10.jpg')
+left_img = cv2.imread(f'{PHOTOS_PATH}/calibration_test.png')
 
 left_img_corr = cv2.undistort(left_img, cameraMatrix=left_camera_matrix, distCoeffs=left_dist_coeffs)
 
-cv2.imshow('left - original', left_img)
-cv2.imshow('left - corrected', left_img_corr)
-cv2.imshow('left - diff', left_img_corr - left_img)
+cv2.imshow('left-original', left_img)
+cv2.imshow('left-corrected', left_img_corr)
+cv2.imshow('left-diff', (255 - (left_img - left_img_corr)))
 
 while True:
     key = cv2.waitKey(5)
